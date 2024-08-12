d2b15f8e05d8923f94bc5cd8e8096447d4bb4079
ArmandoSJ <just-jauregui@hotmail.com>
2024-08-11 20:46:17 -0600

    Se agregran ejemplos de filter y flatmap

---
+++ src/test/java/com/technologyos/reactive/FundamentalsTest.java
@@ -0,0 +1,13 @@
+package com.technologyos.reactive;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FundamentalsTest {
+
+    @Test
+    public void validate_flux(){
+        Fundamentals.fluxExample();
+    }
+}
